b323edb1017ab88b5a304f2f06f3328955a821c0
learning-chip <jiawei.zhuang@outlook.com>
2026-03-03 23:33:06 +0100

    disable `pto.barrier("STORE_VEC")` for now due to https://github.com/zhangstevenunity/PTOAS/pull/187

--- examples/aot/fast_hadamard/hadamard_builder.py
+++ examples/aot/fast_hadamard/hadamard_builder.py
@@ -169,8 +169,9 @@ def build_fast_hadamard(fn_name="fast_hadamard_fp16", manual_sync=False):
                                                     "VEC", "STORE_VEC", event_id=0
                                                 )
                                             pto.store(tb_first, sv_first)
-                                            if manual_sync:
-                                                pto.barrier("STORE_VEC")
+                                            # `pto.barrier("STORE_VEC")` needs fix https://github.com/huawei-csl/pto-dsl/pull/52
+                                            # if manual_sync:
+                                            #     pto.barrier("STORE_VEC")
                                             pto.store(tb_second, sv_second)
                                             if manual_sync:
                                                 pto.record_event(
@@ -234,8 +235,9 @@ def build_fast_hadamard(fn_name="fast_hadamard_fp16", manual_sync=False):
                                                         "VEC", "STORE_VEC", event_id=1
                                                     )
                                                 pto.store(tb_first, sv_first)
-                                                if manual_sync:
-                                                    pto.barrier("STORE_VEC")
+                                                # `pto.barrier("STORE_VEC")` needs fix https://github.com/huawei-csl/pto-dsl/pull/52
+                                                # if manual_sync:
+                                                #     pto.barrier("STORE_VEC")
                                                 pto.store(tb_second, sv_second)
                                                 if manual_sync:
                                                     pto.record_event(
