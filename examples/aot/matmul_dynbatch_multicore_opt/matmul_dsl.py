d447f3b3f7eb2fe8161656a349dffc625c8543ec
fiskrt <43207511+fiskrt@users.noreply.github.com>
2026-03-04 15:16:50 +0000

    feat: double buffer matmul pto-dsl

--- examples/aot/matmul_dynbatch_multicore_opt/matmul_dsl.py
+++ examples/aot/matmul_dynbatch_multicore_opt/matmul_dsl.py
@@ -42,6 +42,24 @@ def build(M=128, K=128, N=128):
 
     const = pto.const
 
+
+    # Until we have set_dyn_flag with event_id as SSA values
+    # event_id can be dynamic SSA value
+    # https://github.com/zhangstevenunity/PTOAS/pull/176
+    def record_event(src, dst, event_id):
+        pto.cond(
+            event_id == const(0),
+            lambda: pto.record_event(src, dst, event_id=0),
+            lambda: pto.record_event(src, dst, event_id=1)
+        )
+     
+    def wait_event(src, dst, event_id):
+        pto.cond(
+            event_id == const(0),
+            lambda: pto.wait_event(src, dst, event_id=0),
+            lambda: pto.wait_event(src, dst, event_id=1)
+        )   
+
     @to_ir_module(meta_data=meta_data)
     def RunTMATMULSplitK(
         out_ptr: "ptr_type",
@@ -61,11 +79,14 @@ def build(M=128, K=128, N=128):
             batch = pto.index_cast(batch_i32)
 
             num_blocks = pto.index_cast(pto.get_block_num())
+            # TODO round robin
             batches_per_core = pto.ceil_div(batch, num_blocks)
             bid = pto.index_cast(pto.get_block_idx())
             b_start = bid * batches_per_core
             b_end_unclamped = b_start + batches_per_core
             b_end = pto.min_u(b_end_unclamped, batch)
+
+            # TODO: if no batched assigned to this core, do nothing...
 
             tvA = pto.as_tensor(tensor_type3d, ptr=a_ptr, shape=[batch, cM, cK], strides=[cK*cM, cK, c1])
             tvC = pto.as_tensor(tensor_type3d, ptr=out_ptr, shape=[batch, cM, cN], strides=[cM*cN, cN, c1])
@@ -85,7 +106,18 @@ def build(M=128, K=128, N=128):
             pto.load(svB, bMatTile)
             pto.record_wait_pair("LOAD", "MOV_M2L", event_id=0)
             pto.mov(bMatTile, bTile)
-            # TODO: wait here so we can use full l1 memory later for A/C.
+            # TODO: wait here so we can use full l1 memory later for A.
+
+
+            # load in the first tile from GM->L1
+            svA = pto.slice_view(tile_view_a, source=tvA, offsets=[b_start, c0, c0], sizes=[c1, cM, cK])
+            curr = c1 - (b_start % c2)
+            pto.cond(
+                curr == c1,
+                lambda: pto.load(svA, aMatTiles[0]),
+                lambda: pto.load(svA, aMatTiles[1]),
+            )
+            record_event("LOAD", "MOV_M2L", event_id=curr)
 
             # signal to LOAD that L1 can be overwritten
             pto.record_event("MOV_M2L", "LOAD", event_id=[0, 1])
@@ -93,67 +125,59 @@ def build(M=128, K=128, N=128):
             pto.record_event("MATMUL", "MOV_M2L", event_id=[0, 1])
             # signal to MATMUL that it can overwrite L0C
             pto.record_event("STORE_ACC", "MATMUL", event_id=[0, 1])
-            for b_idx in pto.for_range(b_start, b_end, c2):
-                # TODO: is it better to just have one GlobalTensor that holds 2 tiles?
-                svA = [
-                    pto.slice_view(tile_view_a, source=tvA, offsets=[b_idx, c0, c0], sizes=[c1, cM, cK]),
-                    pto.slice_view(tile_view_a, source=tvA, offsets=[b_idx+c1, c0, c0], sizes=[c1, cM, cK])
-                ]
-                svC = [
-                    pto.slice_view(tile_view_c, source=tvC, offsets=[b_idx, c0, c0], sizes=[c1, cM, cN]),
-                    pto.slice_view(tile_view_c, source=tvC, offsets=[b_idx+c1, c0, c0], sizes=[c1, cM, cN])
-                ]
 
-                ########## Load tile A1 and A2 from GM -> L1
-                pto.wait_event("MOV_M2L", "LOAD", event_id=0)
-                pto.load(svA[0], aMatTiles[0])
-                pto.record_event("LOAD", "MOV_M2L", event_id=0)
+            for b_idx in pto.for_range(b_start, b_end, c1):
+                curr = b_idx % c2
+                svA = pto.slice_view(tile_view_a, source=tvA, offsets=[b_idx+c1, c0, c0], sizes=[c1, cM, cK])
+                svC = pto.slice_view(tile_view_c, source=tvC, offsets=[b_idx, c0, c0], sizes=[c1, cM, cN])
 
-                pto.wait_event("MOV_M2L", "LOAD", event_id=1)
-                pto.load(svA[1], aMatTiles[1])
-                pto.record_event("LOAD", "MOV_M2L", event_id=1)
+                ########## Load tile A for iteration i+1 from GM -> L1
+                wait_event("MOV_M2L", "LOAD", event_id=curr)
+                with pto.if_context(b_idx + c1 < b_end):
+                    pto.cond(
+                        curr == c1,
+                        lambda: pto.load(svA, aMatTiles[0]),
+                        lambda: pto.load(svA, aMatTiles[1])
+                    )
+                    record_event("LOAD", "MOV_M2L", event_id=curr)
 
 
                 ########## Move A1 and A2 into L0A
-                pto.wait_event("LOAD", "MOV_M2L", event_id=0)
-                pto.wait_event("MATMUL", "MOV_M2L", event_id=0)
-                pto.mov(aMatTiles[0], aTiles[0])
-                pto.record_event("MOV_M2L", "LOAD", event_id=0)
-                pto.record_event("MOV_M2L", "MATMUL", event_id=0)
+                wait_event("LOAD", "MOV_M2L", event_id=c1 - curr)
+                wait_event("MATMUL", "MOV_M2L", event_id=curr)
+                pto.cond(
+                    curr == c0,
+                    lambda: pto.mov(aMatTiles[0], aTiles[0]),
+                    lambda: pto.mov(aMatTiles[1], aTiles[1])
+                )
+                with pto.if_context(b_idx + c2 < b_end):
+                    record_event("MOV_M2L", "LOAD", event_id=curr)
+                record_event("MOV_M2L", "MATMUL", event_id=curr)
 
-                pto.wait_event("LOAD", "MOV_M2L", event_id=1)
-                pto.wait_event("MATMUL", "MOV_M2L", event_id=1)
-                pto.mov(aMatTiles[1], aTiles[1])
-                pto.record_event("MOV_M2L", "LOAD", event_id=1)
-                pto.record_event("MOV_M2L", "MATMUL", event_id=1)
 
                 ########## Perform matmul
-                pto.wait_event("MOV_M2L", "MATMUL", event_id=0)
-                pto.wait_event("STORE_ACC", "MATMUL", event_id=0)
-                pto.matmul(aTiles[0], bTile, cTiles[0])
-                pto.record_event("MATMUL", "STORE_ACC", event_id=0)
-                pto.record_event("MATMUL", "MOV_M2L", event_id=0)
+                wait_event("MOV_M2L", "MATMUL", event_id=curr)
+                wait_event("STORE_ACC", "MATMUL", event_id=curr)
+                pto.cond(
+                    curr == c0,
+                    lambda: pto.matmul(aTiles[0], bTile, cTiles[0]),
+                    lambda: pto.matmul(aTiles[1], bTile, cTiles[1]),
+                )
+                record_event("MATMUL", "STORE_ACC", event_id=curr)
+                with pto.if_context(b_idx + c2 < b_end):
+                    record_event("MATMUL", "MOV_M2L", event_id=curr)
 
-                pto.wait_event("MOV_M2L", "MATMUL", event_id=1)
-                pto.wait_event("STORE_ACC", "MATMUL", event_id=1)
-                pto.matmul(aTiles[1], bTile, cTiles[1])
-                pto.record_event("MATMUL", "STORE_ACC", event_id=1)
-                pto.record_event("MATMUL", "MOV_M2L", event_id=1)
 
                 ######### Store
-                pto.wait_event("MATMUL", "STORE_ACC", event_id=0)
-                pto.store(cTiles[0], svC[0])
-                pto.record_event("STORE_ACC", "MATMUL", event_id=0)
+                wait_event("MATMUL", "STORE_ACC", event_id=curr)
+                pto.cond(
+                    curr == c0,
+                    lambda: pto.store(cTiles[0], svC),
+                    lambda: pto.store(cTiles[1], svC),
+                )
+                with pto.if_context(b_idx + c2 < b_end):
+                    record_event("STORE_ACC", "MATMUL", event_id=curr)
 
-                pto.wait_event("MATMUL", "STORE_ACC", event_id=1)
-                pto.store(cTiles[1], svC[1])
-                pto.record_event("STORE_ACC", "MATMUL", event_id=1)
-
-
-            # if batches not even, then these wait events might wait forever cuz they haven't been set
-            pto.wait_event("MOV_M2L", "LOAD", event_id=[0, 1])
-            pto.wait_event("MATMUL", "MOV_M2L", event_id=[0, 1])
-            pto.wait_event("STORE_ACC", "MATMUL", event_id=[0, 1])
 
     return RunTMATMULSplitK
 
