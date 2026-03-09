7af9f87c4b3e4392afef065ac7029ea87656ea2f
learning-chip <jiawei.zhuang@outlook.com>
2026-03-09 09:30:00 +0000

    generate cpp and ir

---
+++ examples/aot/matmul_swizzle/debug_alloc_tile/withaddr_level3.cpp
@@ -0,0 +1,11 @@
+#include "pto/pto-inst.hpp"
+using namespace pto;
+__global__ AICORE void kernel(__gm__ half* v1) {
+  using T = float;
+
+  #if defined(__DAV_CUBE__)
+  #endif // __DAV_CUBE__
+
+  return;
+}
+
