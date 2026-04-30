a20c7d8ad7a2911969a7991436b31b41a176890d
learning-chip <jiawei.zhuang@outlook.com>
2026-04-30 08:09:45 +0000

    generate cpp from IR

--- examples/aot/flash_attention/ir_ref/README.md
+++ examples/aot/flash_attention/ir_ref/README.md
@@ -1,2 +1,6 @@
 Copy IR from https://github.com/hw-native-sys/PTOAS/pull/609
 to reverse engineer python frontend
+
+```bash
+bash ./gen_cpp.sh  
+```
---
+++ examples/aot/flash_attention/ir_ref/gen_cpp.sh
@@ -0,0 +1,2 @@
+ptoas --pto-arch=a3 --pto-level=level3 --enable-insert-sync fa.pto > fa.cpp
+ptoas --pto-arch=a3 --pto-level=level3 --enable-insert-sync fa_perf.pto > fa_perf.cpp
